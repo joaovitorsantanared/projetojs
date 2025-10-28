a402201f54b6d7d75cd652f6ad5cbbf3b165abac
João Vitor <jvtreloso@gmail.com>
2025-10-28 19:40:17 -0300

    update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-15">
       <div className="w-full max-w-sm md:max-w-2xl">
-        <LoginForm />
+        <LoginForm /> {/* adiciona o formulario*/ }
       </div>
     </div>
   );
