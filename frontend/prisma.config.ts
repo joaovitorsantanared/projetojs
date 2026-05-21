550e603ce0105e42f804efe77631b29f62d6bc14
João Vitor <jvtreloso@gmail.com>
2026-05-21 20:32:56 -0300

    Update prisma.config.ts

--- frontend/prisma.config.ts
+++ frontend/prisma.config.ts
@@ -4,8 +4,11 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
   datasource: {
-    url: process.env.DATABASE_URL!,
-    directUrl: process.env.DIRECT_URL!,
+    url: process.env["DATABASE_URL"],
   },
 });
